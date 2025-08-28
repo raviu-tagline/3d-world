5b4b90b6f838d5d43cce6f9101422d80a3582a74
Ravi Tagline <rvu@taglineinfotach.com>
2025-08-28 19:39:04 +0530

    Initiate

--- DECA/decalib/utils/rasterizer/setup.py
+++ DECA/decalib/utils/rasterizer/setup.py
@@ -1,28 +1,24 @@
-# To install, run
+# To install, run 
 # python setup.py build_ext -i
 # Ref: https://github.com/pytorch/pytorch/blob/11a40410e755b1fe74efe9eaa635e7ba5712846b/test/cpp_extensions/setup.py#L62
 
 from setuptools import setup
-# Import CppExtension to build for CPU
-from torch.utils.cpp_extension import BuildExtension, CppExtension
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import os
 
 # USE_NINJA = os.getenv('USE_NINJA') == '1'
-os.environ["CC"] = "clang"
-os.environ["CXX"] = "clang"
+os.environ["CC"] = "gcc-7"
+os.environ["CXX"] = "gcc-7"
 
 USE_NINJA = os.getenv('USE_NINJA') == '1'
 
 setup(
-    # Change the extension name to 'standard_rasterize_cpu'
-    name='standard_rasterize_cpu',
+    name='standard_rasterize_cuda',
     ext_modules=[
-        # Change CUDAExtension to CppExtension
-        CppExtension('standard_rasterize_cpu', [
-            # Use the CPU source files
-            'standard_rasterize_cpu.cpp',
-            'standard_rasterize_cpu_kernel.cc',
+	CUDAExtension('standard_rasterize_cuda', [
+        'standard_rasterize_cuda.cpp',
+        'standard_rasterize_cuda_kernel.cu',
         ])
-    ],
+	],
     cmdclass={'build_ext': BuildExtension.with_options(use_ninja=USE_NINJA)}
 )
